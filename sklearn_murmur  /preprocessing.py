c9981b861e2bfba8c5accd0f0c087e1e94f90e76
jordmundgand <109611636+jordmundgand@users.noreply.github.com>
2024-02-26 19:56:17 +0700

    Update preprocessing.py

--- sklearn_murmur  /preprocessing.py
+++ sklearn_murmur  /preprocessing.py
@@ -1,1 +1,14 @@
-
+class VocPreprocessor(object):
+    def __init__(self, vocabulary, from_iterable=True, sep=' ',lowercase=True):
+        self.vocabulary_=vocabulary
+        self.from_iterable=from_iterable
+        self.sep=sep
+        self.lowercase=lowercase
+    def preprocess(self, text):
+        if not(self.from_iterable):
+            text=text.split(self.sep)
+        out=self.sep.join([self.vocabulary_[i] for i in text])
+        if self.lowercase:
+            return out.lower()
+        else:
+            return out
